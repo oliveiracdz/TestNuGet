ad946534afc9b9a72e4cc855815e3b216a1499f5
Bright Ran <46307996+BrightRan@users.noreply.github.com>
2020-05-15 17:14:55 +0800

    Setup the package first time

---
+++ TestNuGet/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace TestNuGet
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
